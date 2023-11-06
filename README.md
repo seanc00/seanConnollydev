912700a3c978a0ba160bed77614a5c55fc55da3a
Sean Connolly <121255377+seanc00@users.noreply.github.com>
2023-11-06 17:42:14 +0000

    added welcome cont, tried adding vantajs

---
+++ README.md
@@ -0,0 +1,1 @@
+This is where i will be hosting my website of which will showcase my portfolio and skills as well as who I am.
---
+++ script.js
@@ -0,0 +1,15 @@
+vanta.waves({
+  el: "#welcomeContainer",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x737782,
+  shininess: 19.00,
+  waveHeight: 13.00,
+  waveSpeed: 1.30,
+  zoom: 0.97
+})
